ff68eaffcee4579befe6931e9f2544f089bcb314
Renorog <renornew@gmail.com>
2021-08-29 11:13:24 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroProgramaDio.java
@@ -0,0 +1,30 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroProgramaDio {
+
+	public static void main(String[] args) {
+		
+		Livro livro = new Livro();
+		Gato gato = new Gato();
+		
+		
+		
+		System.out.println(gato);
+		System.out.println(livro);
+		
+		/*-int a = 2;
+		int b = 3;
+		System.out.println("Hello World! " + (a + b));*/
+	}
+}
+	
+	
+	class Livro {
+		private String nome;
+		private String npag;
+	}
+
+
+
